d48c4a47a0ed9807cd44ff98e4404f9d99c17fd9
rs <r.sassassin200180@gmail.com>
2025-08-13 19:42:19 +0330

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { base } from "motion/react-client";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  base: process.env.VITE_BASE_PATH || "/proopco_test",
+});
